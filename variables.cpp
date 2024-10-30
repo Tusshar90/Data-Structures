7ceb7de3cdc1ffd66ecc25db02ad17141dc2b734
Tusshar90 <142090487+Tusshar90@users.noreply.github.com>
2024-10-30 09:29:27 +0530

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int a = 23;
+    float b = 34.7;
+    char c = 'H';
+
+    cout << "Value of variable a is : " << a << endl;
+    cout << "Value of variable b is : " << b << endl;
+    cout << "Value of variable c is : " << c << endl;
+    
+
+}
+
+
+
+
