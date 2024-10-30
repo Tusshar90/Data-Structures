c324ec3d480e88d369182eda08ba31196c1db2bb
Tusshar90 <142090487+Tusshar90@users.noreply.github.com>
2024-10-30 09:36:50 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int a;
+    int b;
+
+    cout << "Enter a :";
+    cin >> a;
+
+    cout << "Enter b :";
+    cin >> b;
+
+    int c = a + b;
+
+    cout << "Sum of a & b : " << c << endl;
+
+
+}
+
+
+
+
