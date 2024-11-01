2734ea484e8a285f4f473cac470c1fcd3d04a339
Tusshar90 <142090487+Tusshar90@users.noreply.github.com>
2024-11-01 12:19:05 +0530

    Add files via upload
    This C loop file contain program to print numbers from 0 to n

---
+++ loop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int number;
+    int i = 0;
+
+    cout << "Enter A Number";
+    cin >> number;
+
+    while (i<=number){
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+
+}
+
+
+
+
