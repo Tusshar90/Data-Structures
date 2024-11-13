fc9a99b0f07035d5c9f36400cf8b32512b214a56
Tusshar90 <142090487+Tusshar90@users.noreply.github.com>
2024-11-13 18:51:46 +0530

    Add files via upload
    Another Number pattern

---
+++ number2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int m=4;
+    int num=1;
+    for(int j=1;j<=m;j++){
+
+        int n=4;
+        for(int i=1;i<=n;i++){
+        cout<<num<<" ";
+        }
+        cout<<endl;
+        num++;
+
+
+    }
+}
+
